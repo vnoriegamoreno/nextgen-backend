7fc6f852e4749ed0e5b93f8125494a8bfe4358f0
Vicente Spencer Noriega Moreno <vicente.noriega.sc@gmail.com>
2023-08-24 01:12:13 -0600

    Add middlewares that logs the calls and send the user to a 404 when the route is not found it

--- server.js
+++ server.js
@@ -4,10 +4,22 @@ const inventoryListRoute = require('./routes/inventoryListRoute');
 
 const app = express();
 
+// Middlewares
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  const now = new Date();
+  console.log(`[${now.toISOString()}] ${req.method} ${req.url}`);
+  next();
+});
 
 // Routes
 app.use('/api/inventory-list', inventoryListRoute);
+
+// Route not found
+app.use((req, res, next) => {
+  console.error('There might be an error on your path route, please take a look');
+  return res.sendStatus(404);
+});
 
 const PORT = process.env.PORT || 8080;
 
